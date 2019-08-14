5e8977fb0619e58c7d2f712575418da5656b4563
stay-on-main <kravtsov.zakhar@gmail.com>
2019-08-14 22:50:31 +0300

    parse stm32 file ok

--- src/main.rs
+++ src/main.rs
@@ -1,145 +1,29 @@
-/*
-struct Register {
-    name: String,
-    display_name: String,
-    description: String,
-    address_offset: usize,
-    size: usize,
-    access: u8,
-    reset_value: u32,
-
-}
-
-
-struct Field {
-    name: String,
-    description: String,
-    bit_offset: usize,
-    bit_width: usize,
-}
-*/
-/*
-extern crate xml;
-
-use std::fs::File;
-use std::io::BufReader;
-
-use xml::reader::{EventReader, XmlEvent};
-
-fn indent(size: usize) -> String {
-    const INDENT: &'static str = "    ";
-    (0..size).map(|_| INDENT)
-             .fold(String::with_capacity(size*INDENT.len()), |r, s| r + s)
-}
-
-fn main() {
-    let file = File::open("C:/Users/zahar.kravtsov/Documents/svd/STM32F103.svd").unwrap();
-    let file = BufReader::new(file);
-
-    let parser = EventReader::new(file);
-    //let mut depth = 0;
-    let mut tag_name: String = "".to_string();
-
-    for e in parser {
-        match e {
-            Ok(XmlEvent::StartElement { name, .. }) => {
-                println!("{}+{}", indent(0), name);
-                tag_name = name.local_name;
-                //depth += 1;
-            }
-            Ok(XmlEvent::EndElement { name }) => {
-                tag_name = "".to_string();
-                //depth -= 1;
-                //println!("{}-{}", indent(depth), name);
-            }
-            Ok(XmlEvent::Characters(text)) => {
-                match tag_name.as_str() {
-                    "name" => {
-                        println!("{}", text);
-                        break;
-                    },
-                    _ => {
-
-                    }
-                }
-                //depth -= 1;
-                //println!("{}-{}", indent(depth), name);
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-                break;
-            }
-            _ => {}
-        }
-    }
-}
-*/
-/*
-struct Xml {
-    tag: String,
-    content: String,
-}
-
-impl Xml {
-    fn new(data: String) -> Xml {
-
-    }
-}
-
-//fn xml_parse(s: &str) ->
-
-fn main()
-{
-
-    let xml = "<field>
-              <name>WAITCFG</name>
-              <description>WAITCFG</description>
-              <bitOffset>11</bitOffset>
-              <bitWidth>1</bitWidth>
-            </field>";
-    
-
-}
-*/
-/*
-use std::fs::File;
-use std::io::prelude::*;
-
-fn main() -> std::io::Result<()> {
-    let mut file = File::open("C:/Users/zahar.kravtsov/Documents/svd/STM32F103.svd")?;
-    let mut contents = vec![];
-    file.read_to_end(&mut contents)?;
-
-    for i in 0..10 {
-        println!("{:2x}", contents[i]);
-    }
-
-    //assert_eq!(contents, "Hello, world!");
-    Ok(())
-}
-*/
 use std::fs::File;
 use std::io::prelude::*;
 use xmltree::Element;
 
+#[derive(Clone)]
 struct Field
 {
     name: String,
     description: String,
-    bit_range: String,
+    bit_offset: String,
+    bit_width: String,
 }
 
+#[derive(Clone)]
 struct Register
 {
     name: String,
     description: String,
     address_offset: String,
-    access: String,
+    //access: String,
     reset_value: String,
-    reset_mask: String,
+    //reset_mask: String,
     fields: Vec<Field>,
 }
 
+#[derive(Clone)]
 struct Perepheral
 {
     name: String,
@@ -151,11 +35,16 @@ struct Perepheral
 
 fn fields_get(mut field: Element) -> Field
 {
-    Field {
+    let f = Field {
         name: field.take_child("name").unwrap().text.unwrap(),
         description: field.take_child("description").unwrap().text.unwrap(),
-        bit_range: field.take_child("bitRange").unwrap().text.unwrap(),
-    }
+        bit_offset: field.take_child("bitOffset").unwrap().text.unwrap(),
+        bit_width: field.take_child("bitWidth").unwrap().text.unwrap(),
+    };
+
+    println!("Field: {}", f.name);
+
+    f
 }
 
 fn registers_get(mut register: Element) -> Register
@@ -164,12 +53,29 @@ fn registers_get(mut register: Element) -> Register
         name: register.take_child("name").unwrap().text.unwrap(),
         description: register.take_child("description").unwrap().text.unwrap(),
         address_offset: register.take_child("addressOffset").unwrap().text.unwrap(),
-        access: register.take_child("access").unwrap().text.unwrap(),
+        //access: register.take_child("access").unwrap().text.unwrap(),
         reset_value: register.take_child("resetValue").unwrap().text.unwrap(),
-        reset_mask: register.take_child("resetMask").unwrap().text.unwrap(),
+        //reset_mask: register.take_child("resetMask").unwrap().text.unwrap(),
         fields: Vec::new(),
     };
 
+    println!("Register: {}", r.name);
+
+    match register.take_child("fields") {
+        Some(mut fields) => {
+            loop {
+                match fields.take_child("field") {
+                    Some(field) => {
+                        r.fields.push(fields_get(field));
+                    },
+                    None => { break },
+                }
+            }
+        },
+        None => { },
+    };
+
+    /*
     let mut fields = register.take_child("fields").expect("Can't find name element");
 
     loop {
@@ -180,11 +86,24 @@ fn registers_get(mut register: Element) -> Register
             None => { break },
         }
     }
-
+    */
+    
     r
 }
 
-fn ph(mut peripheral: Element)
+
+fn generate(p: &Perepheral) {
+    let path = p.name.clone() + ".rs";
+    let mut file = File::create(path).unwrap();
+
+    for r in &p.registers {
+        //file.write_all(b"");
+        write!(&mut file, "reg {}", r.name);
+    }
+}
+
+
+fn peripheral_get(mut peripheral: Element) -> Perepheral
 {
     let mut p = Perepheral {
         name: peripheral.take_child("name").unwrap().text.unwrap(),
@@ -193,6 +112,9 @@ fn ph(mut peripheral: Element)
         base_address: peripheral.take_child("baseAddress").unwrap().text.unwrap(),
         registers: Vec::new(),
     };
+
+    
+    println!("Perepheral: {}", p.name);
 
     let mut registers = peripheral.take_child("registers").expect("Can't find name element");
 
@@ -204,23 +126,50 @@ fn ph(mut peripheral: Element)
             None => { break },
         }
     }
-    //println!("{}", name.text.unwrap());
+
+    generate(&p);
+    p
 }
 
+
 fn main() {
-    let mut file = File::open("C:/Users/stay-on-main/Desktop/svd/STM32F103.svd").unwrap();
+    let mut file = File::open("C:/stay-on-main/svd/STM32F103.svd").unwrap();
     let mut contents = vec![];
     file.read_to_end(&mut contents).unwrap();
-
 
     let mut names_element = Element::parse(contents.as_slice()).unwrap();
 
     let mut peripherals = names_element.take_child("peripherals").expect("Can't find name element");
 
+    let mut ph: Vec<Perepheral> = Vec::new();
+
     loop {
         match peripherals.take_child("peripheral") {
-            Some(peripheral) => {
-                ph(peripheral);
+            Some(mut peripheral) => {
+                let mut derived = false;
+
+                for (attribute, value) in &peripheral.attributes {
+                    if *attribute == "derivedFrom".to_string() {
+                        println!("derivedFrom: {}", value);
+                        
+                        for p in &ph {
+                            if &p.name == value {
+                                let mut new_peripheral = p.clone();
+                                new_peripheral.name = value.clone();
+                                new_peripheral.base_address = peripheral.take_child("baseAddress").unwrap().text.unwrap();
+                                ph.push(new_peripheral);
+                                break;
+                            }
+                        }
+
+                        derived = true;
+                        break;
+                    }
+                }
+                
+                if derived == false {
+                    ph.push(peripheral_get(peripheral));
+                }
             },
             None => { break },
         }
