1ac1d96f3a08a24f2491d6a902ebb939700a4128
Zahar Kravtsov <zahar.kravtsov@developex.local>
2019-08-24 15:46:39 +0300

    add derive

--- src/main.rs
+++ src/main.rs
@@ -235,53 +235,99 @@ fn get_methods(access: Option<svd::Access>) -> (bool, bool) {
     (r, w)
 }
 
-fn generate(peripheral: &Vec<svd::peripheral::Peripheral>) {
-    for p in peripheral {
-        println!("Peripheral name: {}", p.name);
 
-        let path = p.name.to_lowercase() + ".rs";
-        let mut file = File::create(path).unwrap();
+
+fn generate(peripheral: &Vec<svd::peripheral::Peripheral>) {
+    for periph in peripheral {
+        let mut p = periph.clone();
+
+        if let Some(derived_name) = &periph.derived_from {
+            println!("Peripheral name: {}, derived from {}", periph.name, derived_name);
+
+            for new_p in peripheral {
+                if &new_p.name == derived_name {
+                    p = periph.derive_from(new_p);
+                    break;
+                }
+            }
+
+        } else {
+            println!("Peripheral name: {}", p.name);
+        }
+
+        let mut content = String::new();
 
         if let Some(registers) = &p.registers {
             for reg in registers {
                 if let svd::RegisterCluster::Register(r) = reg {
-                    writeln!(&mut file, "pub mod {} {{", r.name.to_lowercase()).unwrap();
-                    
+                    let reg_name = r.name.to_lowercase();
+                    content += (format!("pub mod {} {{\r\n", reg_name)).as_str();
+
                     if let Some(fields) = &r.fields {
                         for f in fields {
-                            writeln!(&mut file, "    pub mod {} {{", f.name.to_lowercase()).unwrap();
+                            let field_name = f.name.to_lowercase();
+                            content += (format!("    pub mod {} {{\r\n", field_name)).as_str();
                             
                             let (rd, wr) = get_methods(f.access);
-                            
                             let reg_addr = p.base_address + r.address_offset;
+                            let mask = ((1u64 << f.bit_range.width) - 1) as u32;
 
                             if rd == true {
-                                writeln!(&mut file, "        pub fn get() -> u32 {{").unwrap();
-                                writeln!(&mut file, "            unsafe {{").unwrap();
-                                writeln!(&mut file, "                core::ptr::read_volatile(0x{:x}u32 as *const u32) >> {}", reg_addr, f.bit_range.offset).unwrap();
-                                writeln!(&mut file, "            }}").unwrap();
-                                writeln!(&mut file, "        }}").unwrap();
+                                // GET
+                                content +=          "        pub fn get() -> u32 {\r\n";
+                                content +=          "            unsafe {\r\n";
+                                
+                                let read_str = format!("core::ptr::read_volatile(0x{:X}u32 as *const u32)", reg_addr);
+
+                                //content += (format!("                let mut reg = core::ptr::read_volatile(0x{:X}u32 as *const u32);\r\n", reg_addr)).as_str();
+
+                                if f.bit_range.offset == 0 {
+                                    content += (format!("                {} & 0x{:X}\r\n", read_str, mask)).as_str();
+                                } else {
+                                    content += (format!("                ({} >> {}) & 0x{:X}\r\n", read_str, f.bit_range.offset, mask)).as_str();
+                                }
+
+                                content +=          "            }\r\n";
+                                content +=          "        }\r\n\r\n";
                             }
 
                             if wr == true {
-                                writeln!(&mut file, "        pub fn set(val: u32) {{}}").unwrap();
+                                // SET
+                                content +=          "        pub fn set(val: u32) {\r\n";
+                                content +=          "            unsafe {\r\n";
+
+                                content += (format!("                let mut reg = core::ptr::read_volatile(0x{:X}u32 as *const u32);\r\n", reg_addr)).as_str();
+                                content += (format!("                reg &= 0x{:X}u32;\r\n", !mask)).as_str();
+
+                                if f.bit_range.offset == 0 {
+                                    content += (format!("                reg |= val & 0x{:X};\r\n", mask)).as_str();
+                                } else {
+                                    content += (format!("                reg |= (val & 0x{:X}) << {};\r\n", mask, f.bit_range.offset)).as_str();
+                                }
+
+                                content += (format!("                core::ptr::write_volatile(0x{:X}u32 as *mut u32, reg);\r\n", reg_addr)).as_str();
+                                content +=          "            }\r\n";
+                                content +=          "        }\r\n";
                             }
 
-                            writeln!(&mut file, "    }}").unwrap();
+                            content += "    }\r\n";
                         }
                     }
 
-                    writeln!(&mut file, "}}").unwrap();
-                    writeln!(&mut file, "").unwrap();
+                    content += "}\r\n\r\n";
                 }
             }
         }
+
+        let path = p.name.to_lowercase() + ".rs";
+        let mut file = File::create(path).unwrap();
+        file.write(content.as_bytes()).unwrap();
     }
 }
 
 fn main() {
     let xml = &mut String::new();
-    File::open("D:/stay-on-main/svd/STM32F103.svd").unwrap().read_to_string(xml);
+    File::open("D:/stay-on-main/svd/STM32F103.svd").unwrap().read_to_string(xml).unwrap();
 
     let f = svd::parse(xml).unwrap();
     generate(&f.peripherals);
